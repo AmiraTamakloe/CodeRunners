829697d79f87cff79ea93b67476ebe19de44339a
Michael Banna <michael.banna@polymtl.ca>
2022-01-22 18:55:08 -0500

    mobile toggles

---
+++ website.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return render_template("index.html")
+
+if __name__ == "__main__":
+	app.run(host="localhost", port=80, debug=True)

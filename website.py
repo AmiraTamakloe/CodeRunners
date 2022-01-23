8aef7914fa60288b23ec496f8ffb054f157f3da4
JACOBDUCAS <jducas182@gmail.com>
2022-01-22 21:26:56 -0500

    Update website.py

--- website.py
+++ website.py
@@ -11,11 +11,11 @@ def home():
 def marvel():
 	return render_template("marvel.html")
 #Dict des posters
-posters = {"Iron Man":..., "The Incredible Hulk":..., "Iron Man 2":..., "Thor":..., "Captain America: The First Avenger":..., "Marvel's The Avengers":...,
-              "Iron Man 3":..., "Thor: The Dark World":..., "Captain America: The Winter Soldier":..., "Guardians of the Galaxy":..., "Avengers: Age of Ultron":...,
-              "Ant-Man":...,"Captain America : Civil War":..., "Doctor Strange":..., "Guardians of the Galaxy Vol. 2":..., "Spider-Man: Homecoming":...,
-              "Thor: Ragnarok":..., "Black Panther":..., "Avengers: Infinity War":..., "Ant-Man and the Wasp":..., "Captain Marvel":..., "Avengers: Endgame":...,
-              "Spider-Man: Far From Home":...,"Black Widow":..., "Shang-Chi and the Legend of the Ten Rings":..., "Eternals":..., "Spider-Man: No Way Home":...}
+posters = {"Iron Man":"{{url_for('static', filename='styles/style.css')}}", "The Incredible Hulk":"{{url_for('static', filename='styles/style.css')}}", "Iron Man 2":"{{url_for('static', filename='styles/style.css')}}", "Thor":"{{url_for('static', filename='styles/style.css')}}", "Captain America: The First Avenger":"{{url_for('static', filename='styles/style.css')}}", "Marvel's The Avengers":"{{url_for('static', filename='styles/style.css')}}",
+              "Iron Man 3":"{{url_for('static', filename='styles/style.css')}}", "Thor: The Dark World":"{{url_for('static', filename='styles/style.css')}}", "Captain America: The Winter Soldier":"{{url_for('static', filename='styles/style.css')}}", "Guardians of the Galaxy":"{{url_for('static', filename='styles/style.css')}}", "Avengers: Age of Ultron":"{{url_for('static', filename='styles/style.css')}}",
+              "Ant-Man":"{{url_for('static', filename='styles/style.css')}}","Captain America : Civil War":"{{url_for('static', filename='styles/style.css')}}", "Doctor Strange":"{{url_for('static', filename='styles/style.css')}}", "Guardians of the Galaxy Vol. 2":"{{url_for('static', filename='styles/style.css')}}", "Spider-Man: Homecoming":"{{url_for('static', filename='styles/style.css')}}",
+              "Thor: Ragnarok":"{{url_for('static', filename='styles/style.css')}}", "Black Panther":"{{url_for('static', filename='styles/style.css')}}", "Avengers: Infinity War":"{{url_for('static', filename='styles/style.css')}}", "Ant-Man and the Wasp":"{{url_for('static', filename='styles/style.css')}}", "Captain Marvel":"{{url_for('static', filename='styles/style.css')}}", "Avengers: Endgame":"{{url_for('static', filename='styles/style.css')}}",
+              "Spider-Man: Far From Home":"{{url_for('static', filename='styles/style.css')}}","Black Widow":"{{url_for('static', filename='styles/style.css')}}", "Shang-Chi and the Legend of the Ten Rings":"{{url_for('static', filename='styles/style.css')}}", "Eternals":"{{url_for('static', filename='styles/style.css')}}", "Spider-Man: No Way Home":"{{url_for('static', filename='styles/style.css')}}"}
 
 if __name__ == "__main__":
 	app.run(host="localhost", port=80, debug=True)

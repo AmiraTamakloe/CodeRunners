b0899a0b157129ac2b86094530da3794d9b20a08
JACOBDUCAS <jducas182@gmail.com>
2022-01-22 21:19:45 -0500

    faire dict posters

--- website.py
+++ website.py
@@ -10,6 +10,12 @@ def home():
 @app.route("/marvel.html")
 def marvel():
 	return render_template("marvel.html")
+#Dict des posters
+posters = {"Iron Man":..., "The Incredible Hulk":..., "Iron Man 2":..., "Thor":..., "Captain America: The First Avenger":..., "Marvel's The Avengers":...,
+              "Iron Man 3":..., "Thor: The Dark World":..., "Captain America: The Winter Soldier":..., "Guardians of the Galaxy":..., "Avengers: Age of Ultron":...,
+              "Ant-Man":...,"Captain America : Civil War":..., "Doctor Strange":..., "Guardians of the Galaxy Vol. 2":..., "Spider-Man: Homecoming":...,
+              "Thor: Ragnarok":..., "Black Panther":..., "Avengers: Infinity War":..., "Ant-Man and the Wasp":..., "Captain Marvel":..., "Avengers: Endgame":...,
+              "Spider-Man: Far From Home":...,"Black Widow":..., "Shang-Chi and the Legend of the Ten Rings":..., "Eternals":..., "Spider-Man: No Way Home":...}
 
 if __name__ == "__main__":
 	app.run(host="localhost", port=80, debug=True)
